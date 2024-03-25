2b0ca026fff56b2c9971acc448a14221f17a2ac4
zyx <ramirezian037@gmail.com>
2024-03-25 04:10:40 -0700

    Footer Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,10 @@
-import { C_Header } from "@/components/elements/header/page";
-
 import { CategorySection } from "@/components/sections/landing/category";
 import { HeroSection } from "@/components/sections/landing/hero";
 
 export default function Home() {
   return (
     <>
-      <C_Header />
+
       <HeroSection />
       <CategorySection />
     </>
